0fa04543907cd3bb70d6e21f7ec4024859ca53fa
SimonSchreinermacher <49522875+SimonSchreinermacher@users.noreply.github.com>
2021-08-27 17:18:46 +0200

    Homepage now loads projects from backend and lists all projects in database

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 
 class Home extends React.Component {
 
     constructor(props){
         super(props);
         this.state={
-            projects: [
-                {id: 1234, name: "Project1"}, 
-                {id: 5678, name: "Project2"}],
+            projects: []
         }
     }
 
@@ -16,10 +15,20 @@ class Home extends React.Component {
         this.props.history.push("/create");
     }
 
+    componentDidMount(){
+        axios.get("http://localhost:8080/projects")
+        .then((res) => {
+            this.setState({projects: res.data});
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
     render(){
         const project_list = this.state.projects.map(project => 
-            <div key={project.id.toString()}>
-                <a href={"/project/" + project.id}>{project.name}</a>
+            <div key={project.project_id.toString()}>
+                <a href={"/project/" + project.project_id}>{project.name}</a>
             </div>
             );
 
--- server/src/main/java/com/simonschreinermacher/projectmanager/controller/ProjectController.java
+++ server/src/main/java/com/simonschreinermacher/projectmanager/controller/ProjectController.java
@@ -3,12 +3,14 @@ package com.simonschreinermacher.projectmanager.controller;
 import com.simonschreinermacher.projectmanager.models.Project;
 import com.simonschreinermacher.projectmanager.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProjectController {
 
     @Autowired
