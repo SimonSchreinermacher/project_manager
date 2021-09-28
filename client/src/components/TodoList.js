8ae47a693e6a124115ee778dc61f6b57ea4a1275
SimonSchreinermacher <49522875+SimonSchreinermacher@users.noreply.github.com>
2021-09-28 23:46:32 +0200

    Todos can now be filtered by category

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -2,8 +2,23 @@ import React from 'react';
 import Todo from './Todo.js';
 
 class TodoList extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            filter: "All"
+        }
+    }
+
     render(){
-        const todo_list = this.props.todos.filter(task => {return (task._finished ^ this.props.mode === "Running")}).map(task => 
+        let filtered_todos;
+        if(this.state.filter === "All"){
+            filtered_todos = this.props.todos;
+        }
+        else{
+            filtered_todos = this.props.todos.filter(task => {return task.category === this.state.filter})
+        }
+        const todo_list = filtered_todos.filter(task => {return (task._finished ^ this.props.mode === "Running")}).map(task => 
             <div> 
                 <Todo key = {Math.floor(Math.random() * 10000000)} todo = {task} project_id = {this.props.project_id}></Todo> 
                 <hr></hr>
@@ -11,6 +26,14 @@ class TodoList extends React.Component {
         );
         return(
             <div>
+                <p>Filter:</p>
+                <select onChange={e => this.setState({filter: e.target.value})}>
+                    <option>All</option>
+                    <option>Feature</option>
+                    <option>Bug</option>
+                    <option>Update</option>
+                    <option>Refactor</option>
+                </select>
                 {todo_list}
             </div>
         );
