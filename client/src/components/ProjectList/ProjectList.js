a9d10c01bf198916a52ade37962362ceef617733
SimonSchreinermacher <49522875+SimonSchreinermacher@users.noreply.github.com>
2021-10-14 19:20:30 +0200

    ProjectList can now be filtered by finished and running projects

--- client/src/components/ProjectList/ProjectList.js
+++ client/src/components/ProjectList/ProjectList.js
@@ -11,7 +11,8 @@ class ProjectList extends React.Component {
         super(props);
         this.state={
             projects: [],
-            filter_search: ""
+            filter_search: "",
+            filter_status: "Running"
         }
     }
 
@@ -35,8 +36,15 @@ class ProjectList extends React.Component {
     }
 
     render(){
-        const filtered_todos = this.state.projects.filter(project => {return project.name.includes(this.state.filter_search)})
-        const project_list = filtered_todos.map(project => 
+        let filtered_projects = this.state.projects;
+        if(this.state.filter_status === "Finished"){
+            filtered_projects = this.state.projects.filter(project => project.finished);
+        }
+        else if(this.state.filter_status === "Running"){
+            filtered_projects = this.state.projects.filter(project => !project.finished);
+        }
+        filtered_projects = filtered_projects.filter(project => {return project.name.includes(this.state.filter_search)})
+        const project_list = filtered_projects.map(project => 
             <div class="link-element list-group-item list-group-item-success" key={project.project_id.toString()}>
                 <p class="link-status">{project.finished ? "Finished" : "Running"}</p>
                 <NavLink to={"/project/" + project.project_id}>{project.name}</NavLink>
@@ -49,9 +57,16 @@ class ProjectList extends React.Component {
                 <button class="add-project-button btn btn-primary" type="submit">Add new project</button>
             </form>
             <h1 class= "header">Your projects:</h1>
-            <form class="searchbar-form" >
-                <input class="searchbar form-control" placeholder="Search for projects" onChange={e => this.setState({filter_search: e.target.value})}></input>
-            </form>
+            <div class="projectlist-filter">
+                <select class="projectlist-filter-status" onChange={e => this.setState({filter_status:e.target.value})}>
+                    <option>Running</option>
+                    <option>Finished</option>
+                    <option>All</option>
+                </select>
+                <form class="searchbar-form" >
+                    <input class="searchbar form-control" placeholder="Search for projects" onChange={e => this.setState({filter_search: e.target.value})}></input>
+                </form>
+            </div>
             <div class="project-list list-group">
                 {project_list}
             </div>
