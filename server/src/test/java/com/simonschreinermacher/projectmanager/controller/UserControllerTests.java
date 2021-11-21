4a29e5b9ec56674e29c38a4c8499dda136228d06
SimonSchreinermacher <49522875+SimonSchreinermacher@users.noreply.github.com>
2021-11-21 22:20:15 +0100

    Added a setup for the controller tests

--- server/src/test/java/com/simonschreinermacher/projectmanager/controller/UserControllerTests.java
+++ server/src/test/java/com/simonschreinermacher/projectmanager/controller/UserControllerTests.java
@@ -5,6 +5,7 @@ import com.simonschreinermacher.projectmanager.models.Project;
 import com.simonschreinermacher.projectmanager.models.Todo;
 import com.simonschreinermacher.projectmanager.models.User;
 import com.simonschreinermacher.projectmanager.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,16 +37,33 @@ public class UserControllerTests {
 
     @MockBean
     UserService service;
+
+    User user;
+    Project project1;
+    Project project2;
+    Todo todo1;
+    Todo todo2;
+
+    @BeforeEach
+    public void setup(){
+        todo1 = new Todo(1L, "todo title", "Feature", "Serious", false);
+        todo2 = new Todo(2L, "another todo", "Refactor", "Minor", true);
+        Set<Todo> todos = new HashSet<Todo>();
+        todos.add(todo1);
+        todos.add(todo2);
+        project1 = new Project(1L, "project name", "desc", "Java", LocalDate.now(), LocalDate.now(), false, todos);
+        project2 = new Project(2L, "other project", "other desc", "Java", LocalDate.now(), LocalDate.now(), false, new HashSet<Todo>());
+        Set<Project> projects = new HashSet<Project>();
+        projects.add(project1);
+        projects.add(project2);
+        user = new User(1L, "username", "password", projects);
+        when(service.findUserByAuthentication()).thenReturn(user);
+    }
     
     @Test
     @DisplayName("All projects that belong to the same user should be returned")
     @WithMockUser
     public void loadProjects() throws Exception{
-        Project project = new Project(1L, "project name", "desc", "Java", LocalDate.now(), LocalDate.now(), false, new HashSet<Todo>());
-        Project project2 = new Project(2L, "other project", "other desc", "Java", LocalDate.now(), LocalDate.now(), false, new HashSet<Todo>());
-        User user = new User(1L, "username", "password", Set.of(project, project2));
-        when(service.findUserByAuthentication()).thenReturn(user);
-
         MvcResult mvcResult = mvc.perform(get("/projects"))
                 .andExpect(status().is2xxSuccessful())
                 .andReturn();
@@ -57,11 +75,6 @@ public class UserControllerTests {
     @DisplayName("Only the project matching the id in the url should be returned")
     @WithMockUser
     public void loadSpecificProject() throws Exception{
-        Project project = new Project(1L, "project name", "desc", "Java", LocalDate.now(), LocalDate.now(), false, new HashSet<Todo>());
-        Project project2 = new Project(2L, "other project", "other desc", "Java", LocalDate.now(), LocalDate.now(), false, new HashSet<Todo>());
-        User user = new User(1L, "username", "password", Set.of(project, project2));
-        when(service.findUserByAuthentication()).thenReturn(user);
-
         MvcResult mvcResult = mvc.perform(get("/projects/1"))
                 .andExpect(status().is2xxSuccessful())
                 .andReturn();
@@ -74,11 +87,7 @@ public class UserControllerTests {
     @DisplayName("When the endpoint is accessed with an id that doesnt belong to any project, the api should return 404")
     @WithMockUser
     public void notExistingProjectReturns404() throws Exception{
-        Project project = new Project(1L, "project name", "desc", "Java", LocalDate.now(), LocalDate.now(), false, new HashSet<Todo>());
-        User user = new User(1L, "username", "password", Set.of(project));
-        when(service.findUserByAuthentication()).thenReturn(user);
-
-        mvc.perform(get("/projects/2"))
+        mvc.perform(get("/projects/3"))
                 .andExpect(status().is4xxClientError());
     }
 
@@ -86,8 +95,6 @@ public class UserControllerTests {
     @DisplayName("The user should contain the new project after the post request")
     @WithMockUser
     public void saveNewProject() throws Exception{
-        User user = new User(1L, "someUser", "password", new HashSet<Project>());
-        when(service.findUserByAuthentication()).thenReturn(user);
         Map<String, Object> payload = new HashMap<>();
         payload.put("name", "project2");
         payload.put("description", "desc");
@@ -109,9 +116,6 @@ public class UserControllerTests {
     @DisplayName("The project should contain the new todo after the post request")
     @WithMockUser
     public void saveNewTodo() throws Exception{
-        Project project = new Project(1L, "project name", "desc", "Java", LocalDate.now(), LocalDate.now(), false, new HashSet<Todo>());
-        User user = new User(1L, "someUser", "password", Set.of(project));
-        when(service.findUserByAuthentication()).thenReturn(user);
         Map<String, Object> payload = new HashMap<>();
         payload.put("title", "todo1");
         payload.put("importance", "Serious");
@@ -124,51 +128,36 @@ public class UserControllerTests {
                         .content(content))
                 .andExpect(status().is2xxSuccessful());
         Todo todo = new Todo(null, "todo1", "Update", "Serious", false);
-        assertThat(project.getTodos()).contains(todo);
+        assertThat(project1.getTodos()).contains(todo);
     }
 
     @Test
     @DisplayName("The user should no longer contain the deleted project, but the other project should still exist")
     @WithMockUser
     public void deleteExistingProject() throws Exception{
-        Project project = new Project(1L, "project name", "desc", "Java", LocalDate.now(), LocalDate.now(), false, new HashSet<Todo>());
-        Project project2 = new Project(2L, "other project", "other desc", "Java", LocalDate.now(), LocalDate.now(), false, new HashSet<Todo>());
-        User user = new User(1L, "someUser", "password", Set.of(project, project2));
-        when(service.findUserByAuthentication()).thenReturn(user);
-
         mvc.perform(delete("/projects/1"))
                 .andExpect(status().is2xxSuccessful());
         assertThat(user.getProjects()).contains(project2);
-        assertThat(user.getProjects()).doesNotContain(project);
+        assertThat(user.getProjects()).doesNotContain(project1);
     }
 
     @Test
     @DisplayName("The project should no longer contain the deleted todo, but the other todo should still exist")
     @WithMockUser
     public void deleteExistingTodo() throws Exception{
-        Todo todo1 = new Todo(1L, "todo title", "Feature", "Serious", false);
-        Todo todo2 = new Todo(2L, "another todo", "Refactor", "Minor", true);
-        Project project = new Project(1L, "project name", "desc", "Java", LocalDate.now(), LocalDate.now(), false, Set.of(todo1, todo2));
-        User user = new User(1L, "someUser", "password", Set.of(project));
-        when(service.findUserByAuthentication()).thenReturn(user);
-
         mvc.perform(delete("/projects/1/todos/1"))
                 .andExpect(status().is2xxSuccessful());
-        assertThat(project.getTodos()).contains(todo2);
-        assertThat(project.getTodos()).doesNotContain(todo1);
+        assertThat(project1.getTodos()).contains(todo2);
+        assertThat(project1.getTodos()).doesNotContain(todo1);
     }
 
     @Test
     @DisplayName("Only the edited details should be changed, and the project should still belong to the same user")
     @WithMockUser
     public void editExistingProject() throws Exception{
-        Project project = new Project(1L, "old project name", "old desc", "Java", LocalDate.of(2021, 11,21), LocalDate.of(2021, 12, 24), false, new HashSet<Todo>());
-        User user = new User(1L, "someUser", "password", Set.of(project));
-        when(service.findUserByAuthentication()).thenReturn(user);
-
         Map<String, Object> payload = new HashMap<>();
         payload.put("name", "new project name");
-        payload.put("description", "old desc");
+        payload.put("description", "new desc");
         payload.put("language", "Java");
         payload.put("deadline", "2021-12-24");
         payload.put("createdOn", "2021-11-21");
@@ -182,25 +171,20 @@ public class UserControllerTests {
                         .content(content))
                 .andExpect(status().is2xxSuccessful());
 
-        assertThat(user.getProjects()).contains(project);
-        assertThat(project.getProject_id()).isEqualTo(1L);
-        assertThat(project.getName()).isEqualTo("new project name");
-        assertThat(project.getDescription()).isEqualTo("old desc");
-        assertThat(project.getLanguage()).isEqualTo("Java");
-        assertThat(project.getCreated_on()).isEqualTo(LocalDate.of(2021, 11, 21));
-        assertThat(project.getDeadline()).isEqualTo(LocalDate.of(2021, 12, 24));
-        assertThat(project.isFinished()).isEqualTo(true);
+        assertThat(user.getProjects()).contains(project1);
+        assertThat(project1.getProject_id()).isEqualTo(1L);
+        assertThat(project1.getName()).isEqualTo("new project name");
+        assertThat(project1.getDescription()).isEqualTo("new desc");
+        assertThat(project1.getLanguage()).isEqualTo("Java");
+        assertThat(project1.getCreated_on()).isEqualTo(LocalDate.of(2021, 11, 21));
+        assertThat(project1.getDeadline()).isEqualTo(LocalDate.of(2021, 12, 24));
+        assertThat(project1.isFinished()).isEqualTo(true);
     }
 
     @Test
     @DisplayName("Only the edited details should be changed, and the todo should still belong to the same project")
     @WithMockUser
     public void editExistingTodo() throws Exception{
-        Todo todo = new Todo(1L, "old title", "Feature", "Minor", false);
-        Project project = new Project(1L, "old project name", "old desc", "Java", LocalDate.of(2021, 11,21), LocalDate.of(2021, 12, 24), false, Set.of(todo));
-        User user = new User(1L, "someUser", "password", Set.of(project));
-        when(service.findUserByAuthentication()).thenReturn(user);
-
         Map<String, Object> payload = new HashMap<>();
         payload.put("title", "new title");
         payload.put("category", "Feature");
@@ -215,12 +199,12 @@ public class UserControllerTests {
                         .content(content))
                 .andExpect(status().is2xxSuccessful());
 
-        assertThat(project.getTodos()).contains(todo);
-        assertThat(todo.getTodo_id()).isEqualTo(1L);
-        assertThat(todo.getTitle()).isEqualTo("new title");
-        assertThat(todo.getCategory()).isEqualTo("Feature");
-        assertThat(todo.getImportance()).isEqualTo("Major");
-        assertThat(todo.get_finished()).isEqualTo(true);
+        assertThat(project1.getTodos()).contains(todo1);
+        assertThat(todo1.getTodo_id()).isEqualTo(1L);
+        assertThat(todo1.getTitle()).isEqualTo("new title");
+        assertThat(todo1.getCategory()).isEqualTo("Feature");
+        assertThat(todo1.getImportance()).isEqualTo("Major");
+        assertThat(todo1.get_finished()).isEqualTo(true);
     }
 
 }
